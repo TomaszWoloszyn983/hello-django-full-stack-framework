c47fc765cc3f34c5bdb01d138a101634b06659d4
Tomasz <tomaszwoloszyn983@gmail.com>
2022-09-09 12:48:24 +0000

    Display the Items from database in html template.

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from .models import Item
 
 # Create your views here.
+
+
 def get_todo_list(request):
+    items = Item.objects.all()
+    context = {
+        'items': items 
+    }
     # return HttpResponse()
-    return render(request, "todo/todo_list.html")
+    return render(request, "todo/todo_list.html", context)
